3e97c64e2f91d960a046c844a6c763b247ac4745
R <rqou@berkeley.edu>
2024-05-19 15:47:26 +0100

    use import statement in main js

--- js/index.js
+++ js/index.js
@@ -1,25 +1,22 @@
 import '../styles/style.scss'
 import * as bootstrap from 'bootstrap';
+import { TileCorruptorAppInst } from '../pkg/index.js';
 
-import("../pkg/index.js").then((rust) => {
-    const { TileCorruptorAppInst } = rust;
+let rust_app_inst = undefined;
 
-    let rust_app_inst = undefined;
+async function choose_new_file(e) {
+    let file = e.target.files[0];
+    let file_data = await file.arrayBuffer();
+    let file_data_u8 = new Uint8Array(file_data);
 
-    async function choose_new_file(e) {
-        let file = e.target.files[0];
-        let file_data = await file.arrayBuffer();
-        let file_data_u8 = new Uint8Array(file_data);
-
-        if (rust_app_inst !== undefined) {
-            rust_app_inst.free();
-        }
-        rust_app_inst = new TileCorruptorAppInst(file_data_u8);
-        rust_app_inst.resize();
-        rust_app_inst.render();
+    if (rust_app_inst !== undefined) {
+        rust_app_inst.free();
     }
+    rust_app_inst = new TileCorruptorAppInst(file_data_u8);
+    rust_app_inst.resize();
+    rust_app_inst.render();
+}
 
-    document
-        .getElementById("filechooser")
-        .addEventListener("change", choose_new_file);
-}).catch(console.error);
+document
+    .getElementById("filechooser")
+    .addEventListener("change", choose_new_file);
